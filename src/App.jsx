e8e4696bc31ccfafc0798a2c0457c86756bc6d00
unset <unset>
2024-05-16 22:14:12 -0300

    feat: use Layout component in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,6 @@
 import "./App.css";
-import Elefante from "./assets/images/svg/elefante.svg";
-import Girafa from "./assets/images/svg/girafa.svg";
-import Morsa from "./assets/images/svg/morsa.svg";
-import Rena from "./assets/images/svg/rena.svg";
-import TvPikachu from "./assets/images/svg/tvpikachu.svg";
-import Urso from "./assets/images/svg/urso.svg";
-import Banner from "./components/Banner";
+import Layout from "./components/Layout";
+import Pernambuco from "./assets/images/svg/estado-de-pernambuco-og.webp";
 
 export default function App() {
   return (
@@ -15,12 +10,7 @@ export default function App() {
         <p>Front-End Javascript</p>
       </div>
 
-      <Banner text='Elefante' description='Aqui está a descrição' image={Elefante}/>
-      <Banner text='Girafa' description='Aqui está a descrição' image={Girafa}/>
-      <Banner text='Morsa' description='Aqui está a descrição' image={Morsa}/>
-      <Banner text='Rena' description='Aqui está a descrição' image={Rena}/>
-      <Banner text='TvPikachu' description='Aqui está a descrição' image={TvPikachu}/>
-      <Banner text='Urso' description='Aqui está a descrição' image={Urso}/>
+      <Layout tag={'Tagline'} title={'Medium length section heading goes here'} text={'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat.'} buttonLeft={'Button'} buttonRight={'Button'} image={Pernambuco}/>
     </body>
   );
 }
