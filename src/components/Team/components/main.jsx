973ebf0ff2880ffc3b675ac57f4054168df0111f
unset <unset>
2024-05-22 22:20:55 -0300

    feat : moving icons in repository

--- src/components/Team/components/main.jsx
+++
@@ -1,9 +0,0 @@
-export default function Main(props) {
-  return (
-    <div className="main-container">
-      <h4>{props.title}</h4>
-      <p>{props.text}</p>
-      <button className="main-button">{props.button}p</button>
-    </div>
-  );
-}
