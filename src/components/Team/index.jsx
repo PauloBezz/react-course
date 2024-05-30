fff8c6109680fcd580d6563d9347596a63948d6d
unset <paulo.bezerra2057@gmail.com>
2024-05-29 22:22:18 -0300

    feat: add Array with map

--- src/components/Team/index.jsx
+++ src/components/Team/index.jsx
@@ -1,12 +1,108 @@
-import { Team } from './src/pattern/composition'
-import './styles.css';
+import "./styles.css";
+import { Team } from "./src/pattern/composition";
 
-export default function TeamComposition(){
-  return(
+const TeamUsers = [
+  {
+    image: "https://imagensemoldes.com.br/wp-content/uploads/2020/02/Mickey-PNG-48.png",
+    name: "Mickey Mouse",
+    job: "Apresentador",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.",
+    mediaSocial: {
+      linkedin: "https://br.linkedin.com/",
+      x: "https://x.com/",
+      dribble: "https://dribbble.com/",
+    },
+  },
+  {
+    image: "https://imagensemoldes.com.br/wp-content/uploads/2020/02/Mickey-PNG-48.png",
+    name: "Mickey Mouse",
+    job: "Apresentador",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.",
+    mediaSocial: {
+      linkedin: "https://br.linkedin.com/",
+      x: "https://x.com/",
+      dribble: "https://dribbble.com/",
+    },
+  },
+  {
+    image: "https://imagensemoldes.com.br/wp-content/uploads/2020/02/Mickey-PNG-48.png",
+    name: "Mickey Mouse",
+    job: "Apresentador",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.",
+    mediaSocial: {
+      linkedin: "https://br.linkedin.com/",
+      x: "https://x.com/",
+      dribble: "https://dribbble.com/",
+    },
+  },
+  {
+    image: "https://imagensemoldes.com.br/wp-content/uploads/2020/02/Mickey-PNG-48.png",
+    name: "Mickey Mouse",
+    job: "Apresentador",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.",
+    mediaSocial: {
+      linkedin: "https://br.linkedin.com/",
+      x: "https://x.com/",
+      dribble: "https://dribbble.com/",
+    },
+  },
+  {
+    image: "https://imagensemoldes.com.br/wp-content/uploads/2020/02/Mickey-PNG-48.png",
+    name: "Mickey Mouse",
+    job: "Apresentador",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.",
+    mediaSocial: {
+      linkedin: "https://br.linkedin.com/",
+      x: "https://x.com/",
+      dribble: "https://dribbble.com/",
+    },
+  },
+  {
+    image: "https://imagensemoldes.com.br/wp-content/uploads/2020/02/Mickey-PNG-48.png",
+    name: "Mickey Mouse",
+    job: "Apresentador",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.",
+    mediaSocial: {
+      linkedin: "https://br.linkedin.com/",
+      x: "https://x.com/",
+      dribble: "https://dribbble.com/",
+    },
+  },
+];
+
+export default function TeamComposition() {
+  return (
     <div className="team-container">
-      <Team.Section tag={'Tagline'} title={'Our Team'} text={'Lorem ipsum dolor sit amet, consectetur adipiscing elit. '}/>
-      <Team.Content name={'Full name'} job={'Job title'} description={'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.'}/>
-      <Team.Main title={'We’re hiring!'} text={'Lorem ipsum dolor sit amet, consectetur adipiscing elit. '} button={'Open options'}/>
+      <Team.Section
+        tag={"Tagline"}
+        title={"Our Team"}
+        text={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. "}
+      />
+      {TeamUsers.map((user, key) => {
+        return (
+          <Team.Content
+            key={key}
+            image={user.image}
+            name={user.name}
+            job={user.job}
+            description={user.description}
+            linkedin={user.mediaSocial.linkedin}
+            x={user.x}
+            dribble={user.dribble}
+          />
+        );
+      })}
+      <Team.Main
+        title={"We’re hiring!"}
+        text={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. "}
+        button={"Open options"}
+      />
     </div>
-  )
+  );
 }
--- src/components/Team/src/pattern/composition/team-content.composition.jsx
+++ src/components/Team/src/pattern/composition/team-content.composition.jsx
@@ -4,7 +4,7 @@ import { EmptyImage, Linkedin, X, Dribble } from "../../assets/images";
 export default function Content(props) {
   return (
     <div className="content-container">
-      <img src={EmptyImage} alt="icon" />
+      <img src={props.image ? props.image : EmptyImage} alt="icon" />
 
       <div className="content-text">
         <div className="content-title">
@@ -15,16 +15,13 @@ export default function Content(props) {
       </div>
 
       <div className="content-icons">
-        <a href="https://br.linkedin.com/" target="_black">
+        <a href={props.linkedin} target="_blank" rel="noreferrer">
           <img src={Linkedin} alt="Linkedin" />
         </a>
-        <a
-          href="https://x.com/i/flow/login?input_flow_data=%7B%22requested_variant%22%3A%22eyJsYW5nIjoicHQiLCJteCI6IjIifQ%3D%3D%22%7D"
-          target="_black"
-        >
+        <a href={props.x} target="_blank" rel="noreferrer">
           <img src={X} alt="X" />
         </a>
-        <a href="httDribbleiDribbleom/" target="_blank">
+        <a href={props.dribble} target="_blank" rel="noreferrer">
           <img src={Dribble} alt="Dribble" />
         </a>
       </div>
