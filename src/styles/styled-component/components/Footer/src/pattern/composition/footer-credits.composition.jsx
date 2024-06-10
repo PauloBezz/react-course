27cbdddd9c5a91ea838fe63faa210e0be243cb53
Paulo Henrique <paulo.bezerra2057@gmail.com>
2024-06-09 23:45:03 -0300

    feat: created composition pattern for Footer component

---
+++ src/styles/styled-component/components/Footer/src/pattern/composition/footer-credits.composition.jsx
@@ -0,0 +1,18 @@
+
+export default function Credits() {
+    return (
+        <>
+            <div>
+                <div>
+                    <p>© 2023 Relume. All rights reserved.</p>
+                </div>
+                <div>
+                    <button>Privacy Policy</button>
+                    <button>Terms of Service</button>
+                    <button>Cookies Settings</button>
+                </div>
+
+            </div>
+        </>
+    )
+}
