515795e301ee551ae8f2695b62ae7b23b8718980
Paulo Henrique <paulo.bezerra2057@gmail.com>
2024-06-25 23:54:54 -0300

    fix: Sign component

--- src/pages/User/components/Signin/index.jsx
+++ src/pages/User/components/Signin/index.jsx
@@ -2,33 +2,33 @@ import { Button, Input } from "@mui/material";
 import { Link } from "react-router-dom";
 import foto from '../../assets/images/svg/foto.svg';
 import logo from '../../assets/images/svg/leon-logo.svg'
+import './styles.css'
 
 export default function Signin(props) {
     return (
-        <>
-            <section>
+            <section className='container-sign'>
                 <img className="foto" src={foto} />
                 <article className="content">
-                    <img src={logo} alt="logo" />
+                    <img src={logo} alt="logo" className="logo"/>
                     <div className="box">
                         <h1>Sign Up</h1>
 
-                        <div className="input">
+                        <div className="sign-input">
                             <h2>{props.user}</h2>
                             <Input />
                         </div>
 
-                        <div className="input">
+                        <div className="sign-input">
                             <h2>{props.email}</h2>
                             <Input />
                         </div>
 
-                        <div className="input">
+                        <div className="sign-input">
                             <h2>{props.pass}</h2>
                             <Input />
                         </div>
 
-                        <div className="input">
+                        <div className="sign-input">
                             <h2>{props.confirm}</h2>
                             <Input />
                         </div>
@@ -36,11 +36,10 @@ export default function Signin(props) {
                         <Button className="btn-create"><p>Create account</p></Button>
                         <div>
                             <p>Already have an account?</p>
-                            <Link className="link" to={"/login"}>Login here</Link>
+                            <Link className="link-login" to={"/login"}>Login here</Link>
                         </div>
                     </div>
                 </article>
             </section>
-        </>
     )
 }
