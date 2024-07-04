e950ddc979d258e3b8f57b8b8c96da2595a89010
Paulo Henrique <paulo.bezerra2057@gmail.com>
2024-07-04 11:09:20 -0300

    refactor: Navbar component applied on Pokemon page

--- src/pages/Pokemon/src/index.jsx
+++ src/pages/Pokemon/src/index.jsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import React, { useState } from "react";
+import Navbar from "../../../styles/css-modules/components/NavBar";
 
 export default function GetPokemon() {
   const [pokedex, setPokedex] = useState(null);
@@ -17,25 +18,36 @@ export default function GetPokemon() {
   }
 
   return (
-    <div className="flex flex-col items-center justify-center">
-      <h1 className="text-2xl">Procure seu pokemon</h1>
-      <input type="text" onChange={(e) => setPokemon(e.target.value)} />
-      <button
-        className="flex items-center justify-center p-2 border-2 rounded border-black"
-        onClick={handlePokemon}
-      >
-        Buscar
-      </button>
-      {pokedex && (
-        <div className="flex flex-col items-center justify-center gap-y-4">
-          <p className="uppercase">{pokedex.name}</p>
-          <img
-            className="w-40 h-40 border-2 border-green-500 shadow-2xl shadow-green-500 rounded"
-            src={pokedex.sprites.front_default}
-            alt="Pokemon"
-          />
-        </div>
-      )}
-    </div>
+    <>
+      <Navbar
+        one="Teams"
+        two="Cep"
+        three="Pokemon"
+        four="About"
+        five="More"
+        buttonOne="Sing up"
+        buttonTwo="Login"
+      />
+      <div className="flex flex-col items-center justify-center">
+        <h1 className="text-2xl">Procure seu pokemon</h1>
+        <input type="text" onChange={(e) => setPokemon(e.target.value)} />
+        <button
+          className="flex items-center justify-center p-2 border-2 rounded border-black"
+          onClick={handlePokemon}
+        >
+          Buscar
+        </button>
+        {pokedex && (
+          <div className="flex flex-col items-center justify-center gap-y-4">
+            <p className="uppercase">{pokedex.name}</p>
+            <img
+              className="w-40 h-40 border-2 border-green-500 shadow-2xl shadow-green-500 rounded"
+              src={pokedex.sprites.front_default}
+              alt="Pokemon"
+            />
+          </div>
+        )}
+      </div>
+    </>
   );
 }
