bdbd80868f8b1d05ec9b0d1488f5f98897387f2b
Paulo Henrique <paulo.bezerra2057@gmail.com>
2024-07-28 23:57:28 -0300

    refactor: Blog component

--- src/components/Blog/src/index.jsx
+++ src/components/Blog/src/index.jsx
@@ -64,7 +64,6 @@ export default function BlogComposition() {
                 name={info.person.name}
                 date={info.person.date}
                 minutes={info.person.minutes}
-
               />
             )
           })}
