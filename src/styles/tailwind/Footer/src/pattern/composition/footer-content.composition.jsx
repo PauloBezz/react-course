f83fde86fe9a243b3f41870972d7ff3aba255efc
Paulo Henrique <paulo.bezerra2057@gmail.com>
2024-07-29 23:35:53 -0300

    refactor: Footer component

--- src/styles/tailwind/Footer/src/pattern/composition/footer-content.composition.jsx
+++ src/styles/tailwind/Footer/src/pattern/composition/footer-content.composition.jsx
@@ -2,17 +2,17 @@ import logo from "../../assets/wing.png"
 
 export default function Content() {
     return (
-        <div className="flex flex-col w-full items-center justify-center mt-20">
-            <div className="w-16 pb-8">
+        <section className="flex flex-col w-full items-center justify-center mt-20">
+            <a className="w-16 pb-8" href="/">
                 <img src={logo} alt="logo" />
-            </div>
-            <div className="flex flex-wrap gap-x-4">
-                <button className="px-2">Link One</button>
-                <button className="px-2">Link Two</button>
-                <button className="px-2">Link Three</button>
-                <button className="px-2">Link Four</button>
-                <button className="px-2">Link Five</button>
-            </div>
-        </div>
+            </a>
+            <article className="flex flex-wrap gap-x-4 ">
+                <button className="px-2 hover:bg-black hover:px-2 hover:rounded border-black hover:text-white">Link One</button>
+                <button className="px-2 hover:bg-black hover:px-2 hover:rounded border-black hover:text-white">Link Two</button>
+                <button className="px-2 hover:bg-black hover:px-2 hover:rounded border-black hover:text-white">Link Three</button>
+                <button className="px-2 hover:bg-black hover:px-2 hover:rounded border-black hover:text-white">Link Four</button>
+                <button className="px-2 hover:bg-black hover:px-2 hover:rounded border-black hover:text-white">Link Five</button>
+            </article>
+        </section>
     )
 }
