78a0f6ca8d8fce35a96760f5dae5e94f202e22eb
Paulo Henrique <paulo.bezerra2057@gmail.com>
2024-08-02 23:54:47 -0300

    refactor: About page

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,5 +1,6 @@
 import Navbar from "../../styles/css-modules/components/NavBar"
 import imagem from "./assets/images/myself.png"
+import './styles.css'
 
 export default function About() {
     return (
@@ -13,15 +14,19 @@ export default function About() {
                 buttonOne="Sing up"
                 buttonTwo="Login"
             />
+
             <section className="about-container">
-                <h1>Developer</h1>
-                <img src={imagem} alt="" />
-                <h2>Técnico em Desenvolvimento de Sistemas</h2>
-                <p>Agradeço pela atenção em ver o meu trabalho do curso de React</p>
+                <article className="about-content">
 
-
+                <h1>Developer - Paulo Bezerra</h1>
+                <img src={imagem} alt="Eu mesmo" title="Minha foto" className="about-image" />
+                
+                <div className="about-text">
+                    <h2>Técnico em Desenvolvimento de Sistemas</h2>
+                    <p>Agradeço pela atenção em ver o meu trabalho do curso de React</p>
+                </div>
+                </article>
             </section>
-
         </>
     )
 }
