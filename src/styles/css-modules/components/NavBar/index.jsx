53b13197d6cc0538ded907e0e6c85aad534d475e
Paulo Henrique <paulo.bezerra2057@gmail.com>
2024-08-04 23:55:31 -0300

    fix: Navbar component

--- src/styles/css-modules/components/NavBar/index.jsx
+++ src/styles/css-modules/components/NavBar/index.jsx
@@ -1,8 +1,8 @@
 import styles from "./styles.module.css";
 import { useState } from "react";
 import { up, down } from "./svg/index";
-import wing from "./svg/wing.png";
 import { Link } from "react-router-dom";
+import wing from "./svg/wing.png";
 import DropMenu from "../DropMenu/src";
 import MenuIcon from "./svg/Menu";
 import DropNav from "../DropNav";
@@ -22,36 +22,32 @@ export default function Navbar(props) {
   };
 
   return (
-    <>
-      <section className={styles.container}>
-        <a href="/">
-          <img src={wing} alt="Logo" className={styles.logo} />
-        </a>
+    <section className={styles.container}>
+      <a href="/">
+        <img src={wing} alt="Logo" className={styles.logo} />
+      </a>
 
-        <button className={styles.btnMenu} onClick={handleNav}><MenuIcon /></button>
+      <button className={styles.btnMenu} onClick={handleNav}><MenuIcon /></button>
 
-        {nav && <DropNav />}
+      {nav && <DropNav />}
+      <div className={styles.column}>
+        <article className={styles.links}>
+          <Link className={styles.link} to={"/teams"}>{props.one}</Link>
+          <Link className={styles.link} to={"/cep"}>{props.two}</Link>
+          <Link className={styles.link} to={"/pokemon"}>{props.three}</Link>
+          <Link className={styles.link} to={"/about"}>{props.four}</Link>
+          <Link className={styles.menuDown} onClick={handleAngle}>
+            {props.five}
+            <img src={Angle} alt="angle" />
+            {visible && <DropMenu />}
+          </Link>
+        </article>
+        <article className={styles.buttons}>
+          <Link className={styles.link} to={"/sign"}>{props.buttonOne}</Link>
+          <Link className={styles.link} to={"/login"}>{props.buttonTwo}</Link>
+        </article>
+      </div>
 
-
-        <div className={styles.column}>
-          <article className={styles.links}>
-            <Link className={styles.link} to={"/teams"}>{props.one}</Link>
-            <Link className={styles.link} to={"/cep"}>{props.two}</Link>
-            <Link className={styles.link} to={"/pokemon"}>{props.three}</Link>
-            <Link className={styles.link} to={"/about"}>{props.four}</Link>
-            <Link className={styles.menuDown} onClick={handleAngle}>
-              {props.five}
-              <img src={Angle} alt="angle" />
-              {visible && <DropMenu />}
-            </Link>
-          </article>
-          <article className={styles.buttons}>
-            <Link className={styles.link} to={"/sign"}>{props.buttonOne}</Link>
-            <Link className={styles.link} to={"/login"}>{props.buttonTwo}</Link>
-          </article>
-        </div>
-
-      </section>
-    </>
+    </section>
   );
 }
