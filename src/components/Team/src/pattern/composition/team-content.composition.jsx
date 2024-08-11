90ef21f732bbb220f8ed0d2bed957e14cb960366
Paulo Henrique <paulo.bezerra2057@gmail.com>
2024-08-10 23:53:51 -0300

    fix: semantic HTML in content

--- src/components/Team/src/pattern/composition/team-content.composition.jsx
+++ src/components/Team/src/pattern/composition/team-content.composition.jsx
@@ -3,18 +3,18 @@ import { EmptyImage, Linkedin, X, Dribble } from "../../assets/images";
 
 export default function Content(props) {
   return (
-    <div className="content-container">
+    <article className="content-container">
       <img src={props.image ? props.image : EmptyImage} alt="icon" />
 
-      <div className="content-text">
+      <section className="content-text">
         <div className="content-title">
           <h4>{props.name}</h4>
           <h3>{props.job}</h3>
         </div>
         <p>{props.description}</p>
-      </div>
+      </section>
 
-      <div className="content-icons">
+      <section className="content-icons">
         <a href={props.linkedin} target="_blank" rel="noreferrer">
           <img src={Linkedin} alt="Linkedin" />
         </a>
@@ -24,7 +24,7 @@ export default function Content(props) {
         <a href={props.dribble} target="_blank" rel="noreferrer">
           <img src={Dribble} alt="Dribble" />
         </a>
-      </div>
-    </div>
+      </section>
+    </article>
   );
 }
