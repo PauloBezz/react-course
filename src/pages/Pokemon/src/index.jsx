0e0c45c2da00d84591b5c5c81cf2686b96e0a6bb
Paulo Henrique <paulo.bezerra2057@gmail.com>
2024-08-12 17:33:41 -0300

    fix: changing the value of the variable to lowercase avoiding errors in the response

--- src/pages/Pokemon/src/index.jsx
+++ src/pages/Pokemon/src/index.jsx
@@ -31,11 +31,11 @@ export default function GetPokemon() {
 
       <article className="flex w-full h-screen flex-col items-center mt-10 text-center">
         <h1 className="text-2xl mb-20">Esse espaço é uma Pokedex, então comece sua jornada aqui!</h1>
-        <section className="flex flex-wrap flex-col-reverse gap-y-2 items-center bg-red-200 rounded">
+        <section className="flex flex-wrap p-2 flex-col-reverse gap-y-2 items-center bg-red-200 rounded">
           <h1 className="text-xl mb-4">Procure seu pokémon</h1>
 
           <div className="flex w-fit gap-4 flex-wrap justify-around">
-            <input type="text" className="w-56 border-black border-2 rounded pl-1" onChange={(e) => setPokemon(e.target.value)} />
+            <input type="text" className="w-56 border-black border-2 rounded pl-1" onChange={(e) => setPokemon(e.target.value.toLowerCase())} />
             <button
               className="flex bg-green-200 self-end justify-center p-2 border-2 rounded border-black font-bold px-10"
               onClick={handlePokemon}
